438bf50f7a85c34346a1536b3b9d2105de790b7f
Yıldırım Akbal <yildirim.akbal@gmail.com>
2022-02-16 15:04:04 +0300

    Update inverse_of_a_matrix.jl

--- inverse_of_a_matrix.jl
+++ inverse_of_a_matrix.jl
@@ -10,7 +10,7 @@ end
 A = create_random_mat(size_) ### this is the matrix to be inverted
 B = create_random_mat(size_) ### this is the matrix that is going to be the inverse of A
 
-function iterate(A::Array{Float32,2}, B::Array{Float32,2}, n::Int64, lr::Float64)
+function iterate(A::Array{Float32,2}, B::Array{Float32,2}, n::Int64, lr::Float32)
     lr = convert(Float32, lr)
     transpose_ = permutedims(A)
     id = typeof(A)(I, size(A))
@@ -24,4 +24,4 @@ function iterate(A::Array{Float32,2}, B::Array{Float32,2}, n::Int64, lr::Float64
     return B
 end
 
-@time B = iterate(A, B, 100, 0.1) ### if you are lucky and adjust the right learning rate you will get the inverse of A, even can see smurfs if stay calm for a while.
+@time B = iterate(A, B, 100, 0.1f0) ### if you are lucky and adjust the right learning rate you will get the inverse of A, even can see smurfs if stay calm for a while.
